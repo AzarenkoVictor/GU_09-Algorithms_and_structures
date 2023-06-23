7a28969b4c7bb96823dd562459b52538f0c1f478
Victor <114249624+AzarenkoVictor@users.noreply.github.com>
2023-06-23 19:23:22 +0300

    HomeWork 03

---
+++ HW03/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MyList mylist = new MyList();
+        mylist.add(2);
+        mylist.add(20);
+        mylist.add(34);
+        mylist.add(444);
+        mylist.add(54);
+        mylist.add(54);
+        mylist.add(994);
+        mylist.add(594);
+        mylist.add(5410);
+        
+        mylist.print();
+        mylist.reverse();
+        mylist.print();
+
+    }
+}
